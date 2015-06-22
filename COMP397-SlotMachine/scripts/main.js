6402f3f4f79aece3a6fad84db9363e20e9a87715
GKona <gerikona@hotmail.com>
2015-06-22 19:32:37 -0400

    Added reel animations and sounds effects

--- COMP397-SlotMachine/scripts/main.js
+++ COMP397-SlotMachine/scripts/main.js
@@ -1,10 +1,11 @@
-﻿/// <reference path="jqxmenu.js" />
-/// <reference path="jquery.js" />
+﻿/// <reference path="jquery.js" />
+// Disables right-clicking
 document.oncontextmenu = function () {
     return false;
 }
 // !!!
 // Code segment below was taken here http://www.html5canvastutorials.com/tutorials/html5-canvas-image-loader/
+// Allows for multiple types images to be displayed within the canvas 
 function loadImages(sources, callback) {
     var images = {};
     var loadedImages = 0;
@@ -24,6 +25,7 @@ function loadImages(sources, callback) {
     }
 }
 
+// Image sources
 var sources = {
     sevens: 'assets/img/7s.jpg',
     bananas: 'assets/img/bananas.png',
@@ -37,9 +39,10 @@ var sources = {
     timer1: 'assets/img/timer1.png',
     timer2: 'assets/img/timer2.png'
 };
-//Code segment above was taken from http://www.html5canvastutorials.com/tutorials/html5-canvas-image-loader/
-//!!!
+// Code segment above was taken from http://www.html5canvastutorials.com/tutorials/html5-canvas-image-loader/
+// !!!
 
+// Draws initial slot machine on the canvas on load
 window.onload = function () {
     var theCanvas = document.getElementById('myCanvas');
     var print = theCanvas.getContext('2d');
@@ -49,8 +52,9 @@ window.onload = function () {
     sPannel.src = "assets/img/sPannel.png";
     sPannel.onload = drawSPannel;
 
-    //Draws slot machine pannels and text
+    // Draws slot machine pannels and text
     function drawSPannel() {
+        startSnd.play();
         print.drawImage(sPannel, 0, 0, 405, 515);
         loadImages(sources, function (images) {
             print.drawImage(images.qReel, 60, 145, 75, 75);
@@ -70,7 +74,7 @@ window.onload = function () {
         print.fillText(lossNumber, 176, 386);
     }
 
-    //Declaration of all number tracking variables 
+    // Declaration of all number tracking variables 
     var playerMoney = 1000;
     var winnings = 0;
     var jackpot = 5000;
@@ -80,7 +84,7 @@ window.onload = function () {
     var lossNumber = 0;
     var spinResult;
     var fruits = "";
-    var winRatio = 0;
+    var winRatio = 0.00;
     var grapes = 0;
     var bananas = 0;
     var oranges = 0;
@@ -93,15 +97,18 @@ window.onload = function () {
     var reel2 = 160;
     var reel3 = 265;
 
-    //Declaration of all sounds 
+    // Declaration of all sounds 
     var spinSnd = new Audio("assets/snd/spin.wav");
     var winSnd = new Audio("assets/snd/win.wav");
     var loseSnd = new Audio("assets/snd/lose.wav");
     var coinSnd = new Audio("assets/snd/coin.wav");
     var jackpotSnd = new Audio("assets/snd/jackpot.wav");
     var quitSnd = new Audio("assets/snd/quit.wav");
+    var startSnd = new Audio("assets/snd/start.wav");
+    var resetSnd = new Audio("assets/snd/reset.wav");
+    var tickSnd = new Audio("assets/snd/tick.wav");
 
-    /* Utility function to show Player Stats */
+    // Utility function to show Player Stats 
     function showPlayerStats() {
         winRatio = winNumber / turn;
         /*
@@ -117,14 +124,19 @@ window.onload = function () {
         print.fillText(playerBet, 188, 277)
         print.fillText(winnings, 288, 277);
         print.fillText(jackpot, 58, 332);
-        print.fillText((winRatio * 100).toFixed(2), 188, 332);
+        if (winRatio >= 0) {
+            print.fillText((winRatio * 100).toFixed(2), 188, 332);
+        }
+        else {
+            print.fillText(0.00, 188, 332);
+        }
         print.fillText("%", 240, 332)
         print.fillText(turn, 58, 386);
         print.fillText(winNumber, 116, 386);
         print.fillText(lossNumber, 176, 386);
     }
 
-    /* Utility function to reset all fruit tallies */
+    // Utility function to reset all fruit tallies 
     function resetFruitTally() {
         grapes = 0;
         bananas = 0;
@@ -136,20 +148,20 @@ window.onload = function () {
         blanks = 0;
     }
 
-    /* Utility function to reset the player stats */
+    // Utility function to reset the player stats 
     function resetAll() {
         playerMoney = 1000;
         playerBet = 0;
         winnings = 0;
         jackpot = 5000;
-        winRatio = 0;
+        winRatio = 0.00;
         turn = 0;
         playerBet = 0;
         winNumber = 0;
         lossNumber = 0;
     }
 
-    /* Check to see if the player won the jackpot */
+    // Check to see if the player won the jackpot 
     function checkJackPot() {
         /* compare two random values */
         var jackPotTry = Math.floor(Math.random() * 51 + 1);
@@ -161,7 +173,7 @@ window.onload = function () {
         }
     }
 
-    /* Utility function to show a win message and increase player money */
+    // Utility function to show a win message and increase player money
     function showWinMessage() {
         playerMoney += winnings;
         //$("div#winOrLose>p").text("You Won: $" + winnings);
@@ -169,14 +181,14 @@ window.onload = function () {
         checkJackPot();
     }
 
-    /* Utility function to show a loss message and reduce player money */
+    // Utility function to show a loss message and reduce player money 
     function showLossMessage() {
         playerMoney -= playerBet;
         //$("div#winOrLose>p").text("You Lost!");
         resetFruitTally();
     }
 
-    /* Utility function to check if a value falls within a range of bounds */
+    // Utility function to check if a value falls within a range of bounds 
     function checkRange(value, lowerBounds, upperBounds) {
         if (value >= lowerBounds && value <= upperBounds) {
             return value;
@@ -186,8 +198,7 @@ window.onload = function () {
         }
     }
 
-    /* When this function is called it determines the betLine results.
-    e.g. Bar - Orange - Banana */
+    // Utility function to determines the betLine results (e.g. Bar - Orange - Banana)
     function Reels() {
         var betLine = [" ", " ", " "];
         var outCome = [0, 0, 0];
@@ -211,19 +222,19 @@ window.onload = function () {
                     betLine[spin] = "Orange";
                     oranges++;
                     break;
-                case checkRange(outCome[spin], 55, 59): //  7.7% probability
+                case checkRange(outCome[spin], 55, 59): // 7.7% probability
                     betLine[spin] = "Cherry";
                     cherries++;
                     break;
-                case checkRange(outCome[spin], 60, 62): //  4.6% probability
+                case checkRange(outCome[spin], 60, 62): // 4.6% probability
                     betLine[spin] = "Bar";
                     bars++;
                     break;
-                case checkRange(outCome[spin], 63, 64): //  3.1% probability
+                case checkRange(outCome[spin], 63, 64): // 3.1% probability
                     betLine[spin] = "Bell";
                     bells++;
                     break;
-                case checkRange(outCome[spin], 65, 65): //  1.5% probability
+                case checkRange(outCome[spin], 65, 65): // 1.5% probability
                     betLine[spin] = "Seven";
                     sevens++;
                     break;
@@ -232,7 +243,7 @@ window.onload = function () {
         return betLine;
     }
 
-    /* This function calculates the player's winnings, if any */
+    // Utility function to calculate the player's winnings, if any and plays winning or losing soundeffect
     function determineWinnings() {
         if (blanks == 0) {
             if (grapes == 3) {
@@ -310,6 +321,7 @@ window.onload = function () {
         }
     }
 
+    // Utility function to assign images to randomly selected real items
     function reelImgs(spin, reel) {
         switch (spin) {
             case "Seven":
@@ -355,7 +367,7 @@ window.onload = function () {
         }
     }
 
-    //Will retain the reel images after the first spin
+    // Utility function to retain the reel images after every spin and during bet change
     function retainReel() {
         if (fruits == "") {
             loadImages(sources, function (images) {
@@ -371,6 +383,7 @@ window.onload = function () {
         }
     }
 
+    // Utility function for reel spin animation
     function reelSpin() {
         spinSnd.currentTime = 0;
         spinSnd.play();
@@ -427,43 +440,29 @@ window.onload = function () {
         }, 660);
     }
 
-    /* When the player clicks the reset button the game resets player stats */
+    // Event handler for when the player clicks the reset button the game resets player stats 
     $("#resetBtn").click(function () {
+        resetSnd.currentTime = 0;
+        resetSnd.play();
         resetAll();
+        retainReel()
         showPlayerStats();
     });
 
+    // Event handler for when the player clicks the quit button the game asks for fonfirmation and then closes
     $("#quitBtn").click(function () {
-        //quitSnd.currentTime = 0;
-        //quitSnd.play();
-        spinSnd.currentTime = 0;
-        spinSnd.play();
-        print.drawImage(sPannel, 0, 0, 405, 515);
-        loadImages(sources, function (images) {
-            print.drawImage(images.timer2, 60, 145, 75, 75);
-            print.drawImage(images.timer2, 165, 145, 75, 75);
-            print.drawImage(images.timer2, 270, 145, 75, 75);
-        });
-
-        setTimeout(function () {
-            print.drawImage(sPannel, 0, 0, 405, 515);
-            loadImages(sources, function (images) {
-                print.drawImage(images.timer1, 60, 145, 75, 75);
-                print.drawImage(images.timer1, 165, 145, 75, 75);
-                print.drawImage(images.timer1, 270, 145, 75, 75);
-            });
-        }, 330);
-
-        setTimeout(function () {
-            print.drawImage(sPannel, 0, 0, 405, 515);
-            loadImages(sources, function (images) {
-                print.drawImage(images.timer2, 60, 145, 75, 75);
-                print.drawImage(images.timer2, 165, 145, 75, 75);
-                print.drawImage(images.timer2, 270, 145, 75, 75);
-            });
-        }, 660);
+        tickSnd.currentTime = 0;
+        tickSnd.play();
+        if (confirm("Are you sure you want to leave the casino?")) {
+            quitSnd.currentTime = 0;
+            quitSnd.play();
+            setTimeout(function () {
+                window.close();
+            }, 1200);
+        }
     });
 
+    // Event handler for when the player clicks the increase bet button the game current bet is increased by 10
     $("#incBetBtn").click(function () {
         if (playerBet < playerMoney) { playerBet += 10; }
         showPlayerStats();
@@ -472,6 +471,7 @@ window.onload = function () {
         coinSnd.play();
     });
 
+    // Event handler for when the player clicks the decreased bet button the game current bet is decreased by 10
     $("#decBetBtn").click(function () {
         if (playerBet > 0) { playerBet -= 10; }
         showPlayerStats();
@@ -480,38 +480,50 @@ window.onload = function () {
         coinSnd.play();
     });
 
-    /* When the player clicks the spin button the game kicks off */
+    // Event handler for when the player clicks the spin button the game kicks off 
     $("#spinBtn").click(function () {
         //playerBet = $("div#betEntry>input").val();
-        reelSpin()
-        setTimeout(function () {
-            if (playerMoney == 0) {
-                if (confirm("You ran out of Money! \nDo you want to play again?")) {
-                    resetAll();
+        if (playerBet == 0) {
+            tickSnd.currentTime = 0;
+            tickSnd.play();
+            alert("You must first place a bet in order to spin.");
+        }
+        else {
+            reelSpin()
+            //document.getElementById('#spinBtn').invisible = true;
+            setTimeout(function () {
+                if (playerMoney == 0) {
+                    if (confirm("You ran out of Money! \nDo you want to play again?")) {
+                        resetAll();
+                        showPlayerStats();
+                    }
+                }
+                    /*
+                    else if (playerBet > playerMoney) {
+                        alert("You don't have enough Money to place that bet.");
+                    }
+                    else if (playerBet < 0) {
+                        alert("All bets must be a positive $ amount.");
+                    }
+                    */
+                else if (playerBet <= playerMoney) {
+                    spinResult = Reels();
+                    // Pushes each reel result to assing images for display
+                    reelImgs(spinResult[0], reel1);
+                    reelImgs(spinResult[1], reel2);
+                    reelImgs(spinResult[2], reel3);
+                    fruits = spinResult[0] + " - " + spinResult[1] + " - " + spinResult[2];
+                    //$("div#result>p").text(fruits);
+                    determineWinnings();
+                    turn++;
                     showPlayerStats();
                 }
-            }
-            else if (playerBet > playerMoney) {
-                alert("You don't have enough Money to place that bet.");
-            }
-            else if (playerBet < 0) {
-                alert("All bets must be a positive $ amount.");
-            }
-            else if (playerBet <= playerMoney) {
-                spinResult = Reels();
-                reelImgs(spinResult[0], reel1);
-                reelImgs(spinResult[1], reel2);
-                reelImgs(spinResult[2], reel3);
-                fruits = spinResult[0] + " - " + spinResult[1] + " - " + spinResult[2];
-                //$("div#result>p").text(fruits);
-                determineWinnings();
-                turn++;
-                showPlayerStats();
-            }
-            else {
-                alert("Please enter a valid bet amount");
-            }
-        }, 900);
+                /*
+                else {
+                    alert("Please enter a valid bet amount");
+                }
+                */
+            }, 900);
+        }
     });
-
 }
